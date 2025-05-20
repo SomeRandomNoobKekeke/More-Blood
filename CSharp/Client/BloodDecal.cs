e35525a25dbc277469f8a809cc52b4fdd9865e0f
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2025-05-20 18:06:37 +0300

    ok go rework

--- CSharp/Client/BloodDecal.cs
+++ CSharp/Client/BloodDecal.cs
@@ -22,22 +22,22 @@ namespace MoreBlood
       if (hull.Submarine != null) { drawPos += hull.Submarine.DrawPosition; }
       drawPos.Y = -drawPos.Y;
 
-      Mod.Log(Color);
+
 
       spriteBatch.Draw(Sprite.Texture, drawPos, clippedSourceRect, Color * GetAlpha(), 0, Vector2.Zero, Scale, SpriteEffects.None, depth);
 
-      if (GameMain.DebugDraw && affectedSections != null && affectedSections.Count > 0)
-      {
-        Vector2 drawOffset = hull.Submarine == null ? Vector2.Zero : hull.Submarine.DrawPosition;
-        Point sectionSize = affectedSections.First().Rect.Size;
-        Rectangle drawPositionRect = new Rectangle((int)(drawOffset.X + hull.Rect.X), (int)(drawOffset.Y + hull.Rect.Y), sectionSize.X, sectionSize.Y);
+      // if (GameMain.DebugDraw && affectedSections != null && affectedSections.Count > 0)
+      // {
+      //   Vector2 drawOffset = hull.Submarine == null ? Vector2.Zero : hull.Submarine.DrawPosition;
+      //   Point sectionSize = affectedSections.First().Rect.Size;
+      //   Rectangle drawPositionRect = new Rectangle((int)(drawOffset.X + hull.Rect.X), (int)(drawOffset.Y + hull.Rect.Y), sectionSize.X, sectionSize.Y);
 
-        foreach (var section in affectedSections)
-        {
-          // Draw colors
-          GUI.DrawRectangle(spriteBatch, new Vector2(drawPositionRect.X + section.Rect.X, -(drawPositionRect.Y + section.Rect.Y)), new Vector2(sectionSize.X, sectionSize.Y), Color.Red, false, 0.0f, (int)Math.Max(1.5f / Screen.Selected.Cam.Zoom, 1.0f));
-        }
-      }
+      //   foreach (var section in affectedSections)
+      //   {
+      //     // Draw colors
+      //     GUI.DrawRectangle(spriteBatch, new Vector2(drawPositionRect.X + section.Rect.X, -(drawPositionRect.Y + section.Rect.Y)), new Vector2(sectionSize.X, sectionSize.Y), Color.Red, false, 0.0f, (int)Math.Max(1.5f / Screen.Selected.Cam.Zoom, 1.0f));
+      //   }
+      // }
     }
   }
 }
